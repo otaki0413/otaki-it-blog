301c9cb6ed2391f3cd1e49d47d9a50cb08658ee9
otaki0413 <takida0413@gmail.com>
2023-05-03 02:00:26 +0900

    🎨  MantineとTailwindを併用できるように設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,40 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className="mx-auto max-w-prose">
-      <header className="border-b border-gray-300 py-8">
-        <Link href="/">
-          <a className="text-5xl font-bold">おたきのITブログ</a>
-        </Link>
-      </header>
-      <main className="mt-8">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <div className="mx-auto max-w-prose">
+          <header className="border-b border-gray-300 py-8">
+            <Link href="/">
+              <a className="text-5xl font-bold">おたきのITブログ</a>
+            </Link>
+          </header>
+          <main className="mt-8">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </MantineProvider>
+    </>
   );
-}
+};
 
-export default MyApp;
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  important: "#__next",
+  theme: {
+    extend: {},
+    screen: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
+  },
   plugins: [require("@tailwindcss/typography")],
 };
